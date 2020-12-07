426589d17971eb42fa646fe828192c556ecef6ae
DeNova <support@denova.com>
2020-12-07 08:18:06 +0000

    "Version: {version}"

--- source/version.py
+++ source/version.py
@@ -2,7 +2,7 @@
     Current distributed version of safelog.
 
     Copyright 2020 DeNova
-    Last modified: 2020-11-29
+    Last modified: 2020-12-06
 '''
 
-SafelogVersion = '1.7.4'
+SAFELOG_VERSION = '1.7.5'
